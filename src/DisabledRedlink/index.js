abdfeb95b379cf018c1b225e0ca6424e12dad4bb
亚衣琴 <Saoutax@gmail.com>
2025-09-07 12:56:33 +0800

    feat: 移除红链

---
+++ src/DisabledRedlink/index.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('a[href*="action=edit"][href*="redlink=1"]').each(function() {
+        $(this).attr('href', function(_, href) {
+            return href
+                .replace(/([&?])(action=edit|redlink=1)/g, '$1')
+                .replace(/\?&/, '?')
+                .replace(/&&+/g, '&')
+                .replace(/[?&]$/, '');
+        });
+    });
+});
