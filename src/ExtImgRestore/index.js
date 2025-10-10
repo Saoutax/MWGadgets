c5401d96431f836098424232cf29915fa43d87a2
亚衣琴 <Saoutax@gmail.com>
2025-10-10 21:20:35 +0800

    feat: support moe-img-blocked

--- src/ExtImgRestore/index.js
+++ src/ExtImgRestore/index.js
@@ -1,5 +1,5 @@
 $(() => {
-	if ( mw.config.get("wgNamespaceNumber") !== -1 && $(".moe-img-error").length ) {
+	if ( mw.config.get("wgNamespaceNumber") !== -1 && ($(".moe-img-error").length > 0 || $(".moe-img-blocked").length > 0) ) {
 		const title = mw.config.get("wgPageName").replace(/_/g, " ");
 		new mw.Api().post({
 			action: "query",
@@ -7,33 +7,43 @@ $(() => {
 			rvprop: "content",
 			titles: title,
 			formatversion: 2
-		}).then(data => {
+		}).then((data) => {
 			const page = data.query.pages[0];
 			const content = page.revisions?.[0]?.content || "";
 
-			$(".moe-img-error").toArray().forEach(($el, index) => {
-				const $this = $($el);
-				const src = $this.attr("data-src-input");
+			$(".moe-img-error, .moe-img-blocked").each(function (index) {
+				const $this = $(this);
+				const isLink = $this.is("a");
+				const src = isLink ? $this.attr("href") : $this.attr("data-src-input");
 
 				const imgRegex = new RegExp(`<img[^>]*src=["']${src.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")}["'][^>]*>`, "gi");
-				const matches = Array.from(content.matchAll(imgRegex));
-				const targetMatch = matches[index]?.[0];
+				const matches = [...content.matchAll(imgRegex)];
+				const targetMatch = matches[index]?.[0] || null;
 
 				let styleAttr = "";
-				let originalTitle = "";
 				if (targetMatch) {
 					const styleMatch = targetMatch.match(/style=["']([^"']+)["']/i);
 					styleAttr = styleMatch?.[1] || "";
-
-					const titleMatch = targetMatch.match(/title=["']([^"']+)["']/i);
-					originalTitle = titleMatch?.[1] || "";
 				}
 
 				const img = new Image();
-				img.onload = () => {
-					if (styleAttr) img.style.cssText = styleAttr;
-					img.title = originalTitle ? `「ExtImgRestore」${originalTitle}` : "ExtImgRestore";
-					$this.replaceWith(img);
+				img.onload = function () {
+					if (styleAttr) {
+						$(this).attr("style", styleAttr);
+					}
+
+					if (isLink) {
+						const link = $("<a>").attr({
+							href: src,
+							target: $this.attr("target") || "_blank",
+							rel: $this.attr("rel") || "noopener noreferrer",
+							class: $this.attr("class") || "",
+							title: $this.attr("title") || ""
+						}).append(this);
+						$this.replaceWith(link);
+					} else {
+						$this.replaceWith(this);
+					}
 				};
 				img.src = src;
 				img.alt = src;
