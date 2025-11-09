d7cb52e9e8e4865d17b890b987255d935c5307dd
亚衣琴 <Saoutax@gmail.com>
2025-11-09 09:21:38 +0800

    chore:

--- vite.config.js
+++ vite.config.js
@@ -7,27 +7,27 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = resolve(__filename, "..");
 
 function getEntries() {
-  const srcDir = resolve(__dirname, "src");
-  const entries = {};
-  fs.readdirSync(srcDir).forEach((dir) => {
+const srcDir = resolve(__dirname, "src");
+const entries = {};
+fs.readdirSync(srcDir).forEach((dir) => {
     const jsPath = resolve(srcDir, dir, "index.js");
     const tsPath = resolve(srcDir, dir, "index.ts");
     const cssPath = resolve(srcDir, dir, "index.css");
 
     if (fs.existsSync(tsPath)) {
-      entries[dir] = tsPath;
+    entries[dir] = tsPath;
     } else if (fs.existsSync(jsPath)) {
-      entries[dir] = jsPath;
+    entries[dir] = jsPath;
     } else if (fs.existsSync(cssPath)) {
-      entries[dir] = cssPath;
+    entries[dir] = cssPath;
     }
-  });
-  return entries;
+});
+return entries;
 }
 
 export default defineConfig({
-  mode: "production",
-  build: {
+mode: "production",
+build: {
     sourcemap: true,
     outDir: "dist",
     emptyOutDir: true,
@@ -35,19 +35,19 @@ export default defineConfig({
     minify: "esbuild",
     cssCodeSplit: true,
     rollupOptions: {
-      input: getEntries(),
-      output: {
+    input: getEntries(),
+    output: {
         entryFileNames: "[name].min.js",
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+        if (assetInfo.name && assetInfo.name.endsWith(".css")) {
             return "[name].min.[ext]";
-          }
-          return "[name].[ext]";
+        }
+        return "[name].[ext]";
         },
-      },
     },
-  },
-  resolve: {
+    },
+},
+resolve: {
     extensions: [".ts", ".js", ".css"],
-  },
+},
 });
