b7836d4306d9d58a48fd1a29da07e88c51677800
亚衣琴 <Saoutax@gmail.com>
2026-05-09 22:23:43 +0800

    refactor: 将函数改为箭头函数并补充 JSDoc 文档
    - getContent 添加 titles 参数以支持获取指定页面内容
    - statusConsole 中 consoleError 和 consoleSuccess 改为箭头函数
    - 补充 JSDoc 注释

--- src/utils/getContent.ts
+++ src/utils/getContent.ts
@@ -1,6 +1,12 @@
 const { wgPageName } = mw.config.get();
 
-async function getContent() {
+/**
+ * 获取页面内容
+ *
+ * @param titles 页面标题，默认为当前页面
+ * @returns 页面内容
+ */
+const getContent = async (titles = wgPageName) => {
     const {
         query: {
             pages: [
@@ -11,13 +17,12 @@ async function getContent() {
         },
     } = await new mw.Api().get({
         action: "query",
-        titles: wgPageName,
+        titles,
         prop: "revisions",
         rvprop: "content",
         formatversion: 2,
     });
-
     return content;
-}
+};
 
 export { getContent };
--- src/utils/statusConsole.ts
+++ src/utils/statusConsole.ts
@@ -1,13 +1,24 @@
-function consoleError(gadget: string, error: never | unknown): void {
+/**
+ * 提示错误
+ *
+ * @param gadget 小工具名称
+ * @param error 错误信息
+ */
+const consoleError = (gadget: string, error: never | unknown) => {
     mw.notify("发生错误，请于控制台查看详情。", { type: "error" });
     console.log(`[${gadget}] Error: ${JSON.stringify(error)}`);
-}
+};
 
-function consoleSuccess(action: string): void {
+/**
+ * 提示成功
+ *
+ * @param action 操作类型
+ */
+const consoleSuccess = (action: string) => {
     mw.notify(`${action}成功，即将刷新……`, { type: "success" });
     setTimeout(() => {
         location.reload();
     }, 2000);
-}
+};
 
 export { consoleError, consoleSuccess };
